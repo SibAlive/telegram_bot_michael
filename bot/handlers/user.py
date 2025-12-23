60843662bcc93e08f32088409f33d7b48e373786
Anton Roshchupkin <sibalive@mail.ru>
2025-12-23 19:04:38 +0700

    Исправлен формат времени

--- bot/handlers/user.py
+++ bot/handlers/user.py
@@ -148,7 +148,7 @@ async def process_day_choose(callback_query, session, state):
 async def process_time_choose(callback_query, state):
     data = await state.get_data()
     dt = data.get('dt')
-    date_time = dt + callback_query.data
+    date_time = f"{dt} {callback_query.data}"
     await state.update_data(date_time=date_time)
 
     await state.set_state(ServiceFrom.fill_service)
